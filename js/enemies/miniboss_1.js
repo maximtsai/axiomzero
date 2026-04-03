fbd29877ed7cd654adc8bc18037baba7c62d6ab5
Maxim Tsai <maximtsai@yahoo.com>
2026-04-03 00:16:00 -0700

    slower damage scaling

--- js/enemies/miniboss_1.js
+++ js/enemies/miniboss_1.js
@@ -271,7 +271,7 @@ class Miniboss1 extends Miniboss {
             enemyBulletManager.fire(
                 m.x + ux * 27, m.y + uy * 27,
                 targetX, targetY,
-                MB1.BULLET_DAMAGE * (m.multiplier || 1),
+                MB1.BULLET_DAMAGE * (1 + ((m.multiplier || 1) - 1) * 0.75),
                 'bullet.png',
                 null,
                 true,
--- js/enemies/miniboss_2.js
+++ js/enemies/miniboss_2.js
@@ -224,14 +224,14 @@ class Miniboss2 extends Miniboss {
                 // Collision detection
                 if (distToTower <= m.size && !m.hasHitTowerInCurrentAttack) {
                     // Force the manual attack
-                    tower.takeDamage(MB2.DAMAGE * (m.multiplier || 1), m.x, m.y);
+                    tower.takeDamage(MB2.DAMAGE * (1 + ((m.multiplier || 1) - 1) * 0.75), m.x, m.y);
                     if (typeof cameraManager !== 'undefined') {
                         cameraManager.shake(300, 0.02);
                     }
 
                     // Force manual self damage and text display by proxy
                     // We directly take damage since we disabled our native 'damage' stat
-                    enemyManager.damageEnemy(this, MB2.SELF_DAMAGE * (m.multiplier || 1), 'notrecorded');
+                    enemyManager.damageEnemy(this, MB2.SELF_DAMAGE * (1 + ((m.multiplier || 1) - 1) * 0.75), 'notrecorded');
 
                     m.hasHitTowerInCurrentAttack = true;
 
